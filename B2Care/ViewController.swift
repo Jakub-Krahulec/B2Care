3b7c1a9de1ddad43a562768ae088f0b8fb966770
Jakub Krahulec <kuba.kra@email.cz>
2020-12-22 15:54:30 +0100

    Initial commit

---
+++ B2Care/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B2Care
+//
+//  Created by Jakub Krahulec on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'B2Care' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for B2Care
+pod 'Alamofire'
+pod 'SnapKit'
+end
